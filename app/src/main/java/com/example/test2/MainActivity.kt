ca2c4821649b9ca0cb1d3053286179bd1a997b9d
studentDG <daegu776@gmail.com>
2021-11-01 02:46:44 +0900

    코틀린에 주석 달기

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -12,19 +12,22 @@ class MainActivity : AppCompatActivity() {
 
 
        loginBtn.setOnClickListener {
+//         로그인 버튼이 눌렸을때
 
            val inputId = emailEdt.text.toString()
            val inputPw = passwordEdt.text.toString()
 
+//         아이디 비번 변수 선언
+
            if ( inputId == "daegu776@gmail.com" && inputPw =="qwer") {
                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
            }
+//           if문 작성
 
            else {
                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
            }
-
-
+//           조건이 틀렷을때
 
 
 
